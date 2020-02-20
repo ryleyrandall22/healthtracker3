602a7cc0286d88168b341bc2f3d253110d0cac42
Ryley Randall <33163774+ryleyrandall22@users.noreply.github.com>
2020-02-20 11:26:14 -0700

    Completed app without the reminder

---
+++ src/chart.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import {
+  AppBar,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  TextField,
+  Toolbar,
+  Typography,
+  Radio
+} from "@material-ui/core";
+import {Line} from "react-chartjs-2"
+
+import { Link, Route } from "react-router-dom";
+import { db } from "./firebase";
+import moment from "moment";
+
+export default function Chart(props){
+    const [surveys, setSurveys] = useState([])
+    const [labels, setLabels] = useState([])
+    const [dataSets, setDataSets] = useState([])
+
+    useEffect(() => {
+        const unsub = db.collection('users').doc(props.user.uid).collection('surveys').onSnapshot((snapshot) => {
+            const surveys = snapshot.docs.map((doc) => {
+                const survey = {
+                    sleep: parseInt(doc.data().sleep),
+                    happiness: doc.data().happiness,
+                    date: new Date(doc.data().date.seconds*1000),
+                    id:doc.id
+                }
+                return survey
+            })
+            setSurveys(surveys)
+        })
+        return unsub
+    },[])
+
+    useEffect(() => {
+        //get and display labels
+        const lbls = surveys.map((survey) => {
+            return moment(survey.date).format("M/D/YY")
+        })
+        setLabels(lbls)
+
+        //Creating Holder for Data Sets
+        const sets = []
+
+        //Creating the sleep object
+        const sleep = {
+            label:"Hours of sleep",
+            data:surveys.map(s => s.sleep),
+            borderColor:'red',
+            borderWidth:1
+        }
+        
+        sets.push(sleep)
+
+        //Creating Happiness Objects
+        const happiness = {
+            label:"Happiness",
+            data:surveys.map(s => s.happiness),
+            borderColor:'blue',
+            borderWidth:1
+        }
+        sets.push(happiness)
+
+        //Set the State Variable
+        console.log(sets)
+        setDataSets(sets)
+
+
+    },[surveys])
+
+    return(
+        <div style={{display:"flex", justifyContent:'center'}} >
+            <Paper style={{padding:12, marginTop:30, width:'100%', maxWidth:400}}>
+                <Typography variant="h4">Chart</Typography>
+                <Line
+                   data={{
+                    labels:labels,
+                    datasets: dataSets
+                }}
+                />
+
+            </Paper>
+        </div>
+    )
+}
+
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAJBek0Y1qkffJD4lBUe12gFCRbEeRKJh8",
+  authDomain: "healthtracker3-c0cd1.firebaseapp.com",
+  databaseURL: "https://healthtracker3-c0cd1.firebaseio.com",
+  projectId: "healthtracker3-c0cd1",
+  storageBucket: "healthtracker3-c0cd1.appspot.com",
+  messagingSenderId: "817060807654",
+  appId: "1:817060807654:web:6fd0a4b58b46bbb28e4c27"
 };
 
 firebase.initializeApp(config);
